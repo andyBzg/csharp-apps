77c91b43890646df6c6f76dbdc62ee8a9e234fd5
andyBzg <andr.bzg@gmail.com>
2025-04-04 14:26:11 +0200

    add new exercise add extended data type explanations

---
+++ Aufgabe_Schere-Stein-Papier/Program.cs
@@ -0,0 +1,68 @@
+﻿namespace Aufgabe_Schere_Stein_Papier
+{
+    /*Programmiere ein Schere, Stein, Papier Spiel.  
+    Der User soll eines der Handzeichen wählen können. 
+    Der Computer wählt per Zufall ein eigenes Zeichen. 
+    Dem User wird dann ausgegeben, welches Zeichen der Computer gewählt hat und ob man gewonnen, verloren oder ein Unentschieden hat. 
+    Danach kann sich der User entscheiden eine weitere Runde zu spielen, oder nicht. */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            string[] handzeichen = { "Schere", "Stein", "Papier" };
+            bool check, weiterSpielen = true;
+            int userEingabe;
+            string userHandzeichen, zufall;
+
+            while (weiterSpielen)
+            {
+                Console.WriteLine("Bitte wählen Sie Handzeichen (1 - 3):");
+                for (int i = 0; i < handzeichen.Length; i++)
+                {
+                    Console.WriteLine($"[{i + 1}] {handzeichen[i]}");
+                }
+
+                Console.Write("Ihre Wahl: ");
+                check = int.TryParse(Console.ReadLine(), out userEingabe);
+
+                if (check && userEingabe > 0 && userEingabe <= 3)
+                {
+                    userHandzeichen = handzeichen[userEingabe - 1];
+                    zufall = handzeichen[rnd.Next(handzeichen.Length)];
+
+                    if (userHandzeichen == zufall)
+                    {
+                        Console.WriteLine("Unentschieden!");
+                    }
+                    else if (userHandzeichen == "Schere" && zufall == "Papier" ||
+                            userHandzeichen == "Stein" && zufall == "Schere" ||
+                            userHandzeichen == "Papier" && zufall == "Stein")
+                    {
+                        Console.WriteLine($"Sie haben gewonnen! \nComputer hat {zufall} gewählt.");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Sie haben verloren! \nComputer hat {zufall} gewählt.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Fehler: Bitte eine Zahl zwischen 1 und 3 eingeben!");
+                    return;
+                }
+
+                Console.WriteLine("Noch eine Runde? Drücken Sie Enter...");
+                if (Console.ReadKey(true).Key == ConsoleKey.Enter)
+                {
+                    Console.Clear();
+                }
+                else
+                {
+                    Console.WriteLine("Programm beendet.");
+                    weiterSpielen = false;
+                }
+            }
+        }
+    }
+}
--- Variablen_Datentypen/Program.cs
+++ Variablen_Datentypen/Program.cs
@@ -36,6 +36,25 @@
             b1 = false;
 
             // Bezeichner: alle Buchstaben, alle Ziffern, der _, darf nicht mit einer Ziffer beginnen
+
+            byte b;         // 0-255
+            sbyte sb;       // s steht für signed, -128 bis 127
+
+            short sh2;      // 16-bit Ganzzahl, min -32.768, max 32.767
+            ushort ush;     // u steht für unsigned, 0-65.535
+
+            int i2;         // 32-bit Ganzzahl, min -2.147.483.648, max 2.147.483.647
+            uint ui;        // 0-4.294.967.295
+
+            long l2;        // 64-bit Ganzzahl, min -9.223.372.036.854.775.808, max 9.223.372.036.854.775.807
+            ulong ul;       // 0-18.446.744.073.709.551.615
+
+            // Sonstige
+            bool istWahr = true; // 2 Zustände : 1 Bit
+
+            char zeichen = 'A'; // 2 Byte, 16 Bit, Unicode
+
+            string wort = "Hallo"; // Die Größe bemisst sich nach dem Inhalt des Strings | 2 Byte pro Zeichen, 16 Bit pro Zeichen, Unicode
         }
     }
 }
