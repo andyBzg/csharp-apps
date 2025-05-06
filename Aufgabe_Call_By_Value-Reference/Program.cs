e010cf3f4790e53e15f28d6fe4310b1e067eee97
andyBzg <andr.bzg@gmail.com>
2025-05-06 16:02:34 +0200

    add new tasks

---
+++ Aufgabe_Call_By_Value-Reference/Program.cs
@@ -0,0 +1,96 @@
+﻿namespace Aufgabe_Call_By_Value_Reference
+{
+    //Aufgabe 
+    //Erstelle eine Methode die gleichzeitig ref und out verwendet
+    //Diese Methode soll eine boolean zurückgeben gleichzeitig einen string infoText initialisieren mit out
+    //und als ref eine Liste abrufen mit Inventargegenständen
+
+    //Die Methode soll es uns erlauben einen Heiltrank zu trinken des sich in der Liste befindet 
+    //als infotext soll darüber informiert werden ob der Trank konsumiert wurde wenn er vorhanden ist, wenn nicht soll er Sagen das keine Tranke vorhanden sind
+    //zudem soll eine int Lebenspunkte intilisiert werden die wenn ein Trank vorhanden ist um 50pts erht wird
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int lebenspunkte = 25;
+            List<string> inventar = new List<string>() { "Heiltrank", "Heiltrank", "Heiltrank" };
+
+            while (true)
+            {
+                ZeigeZustand(lebenspunkte, inventar);
+                FrageNachTrank(ref inventar, ref lebenspunkte);
+            }
+        }
+
+        private static void FrageNachTrank(ref List<string> inventar, ref int lebenspunkte)
+        {
+            Console.Write("Drücke \"H\", um einen Heiltrank zu trinken ");
+
+            if (Console.ReadKey().Key == ConsoleKey.H)
+            {
+                bool istHeiltrankKonsumiert = RegenerationsSchluck(ref inventar, out string info);
+                Console.WriteLine();
+                ZeigeInfo(info);
+                ErhoeheLebenspunkte(ref lebenspunkte, istHeiltrankKonsumiert);
+            }
+            else
+            {
+                ZeigeInfo("\nDu hast dich entschieden, keinen Heiltrank zu trinken.");
+            }
+
+            WarteSpielerEingabe("Bitte beliebige Taste drücken");
+        }
+
+        private static void WarteSpielerEingabe(string message)
+        {
+            Console.WriteLine();
+            Console.WriteLine(message);
+            Console.ReadKey();
+            Console.Clear();
+        }
+
+        private static void ZeigeZustand(int lebenspunkte, List<string> inventar)
+        {
+            Console.WriteLine($"Lebenspunkte: {lebenspunkte} pts");
+            Console.WriteLine($"Inventargegenstände: {inventar.Count}");
+            Console.WriteLine();
+        }
+
+        private static void ZeigeInfo(string info)
+        {
+            Console.BackgroundColor = ConsoleColor.DarkCyan;
+            Console.WriteLine(info);
+            Console.ResetColor();
+        }
+
+        private static void ErhoeheLebenspunkte(ref int lebenspunkte, bool istHeiltrankKonsumiert)
+        {
+            if (istHeiltrankKonsumiert)
+            {
+                lebenspunkte += 50;
+            }
+        }
+
+        private static bool RegenerationsSchluck(ref List<string> inventar, out string infoText)
+        {
+            string item = "Heiltrank";
+
+            if (inventar.Count == 0)
+            {
+                infoText = "Das Inventar ist leer";
+                return false;
+            }
+            else if (!inventar.Contains(item))
+            {
+                infoText = "Keine Tränke vorhanden";
+                return false;
+            }
+            else
+            {
+                inventar.Remove(item);
+                infoText = "Der Trank wurde konsumiert";
+                return true;
+            }
+        }
+    }
+}
---
+++ Call_by_Reference_Call_by_Value/Program.cs
@@ -0,0 +1,116 @@
+﻿namespace Call_by_Reference_Call_by_Value
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Call by Value
+            CallByValue();
+
+            // Call by Reference
+            CallByReference();
+
+            // Beuen einer eigenen Try Parse Methode
+
+            bool TryParseStringToInt(string? meinText, out int zahl)
+            {
+                zahl = 0;
+                if (meinText == null)                
+                    return false;                
+
+                try
+                {
+                    zahl = Convert.ToInt32(meinText.Trim());
+                    return true;
+                }
+                catch
+                {
+                    return false;
+                }
+            }
+
+            void Test(bool hatGeklappt, int eingabe)
+            {
+                if (hatGeklappt)                
+                    Console.WriteLine("Deine Eingabe ist eine Zahl: " + eingabe);                
+                else                
+                    Console.WriteLine("Gebe eine Zahl ein");                
+            }
+
+            string stringOhneZahl = "Das ist ein string";
+            string stringMitZahl = "321";
+
+            bool hatGeklappt = TryParseStringToInt(stringOhneZahl, out int eingabe);
+            Test(hatGeklappt, eingabe);
+
+            hatGeklappt = TryParseStringToInt(stringMitZahl, out eingabe);
+            Test(hatGeklappt, eingabe);
+        }
+
+        private static void CallByReference()
+        {
+            // mit ref-Schlüsselwort
+            int gehalt = 3600;
+
+            void Zusammenrechnen(ref int x)
+            {
+                x += 500;
+            }
+
+            Zusammenrechnen(ref gehalt);
+            Console.WriteLine(gehalt);
+
+            // mit out-Schlüsselwort
+            int gehaltZwei;
+
+            void Abzuege(out int summe, int gehalt)
+            {
+                if (gehalt >= 2500)
+                {
+                    summe = 500;
+                }
+                else if (gehalt < 2500 && gehalt >= 1500)
+                {
+                    summe = 300;
+                }
+                else
+                {
+                    summe = 0;
+                }
+            }
+
+            gehaltZwei = 4400;
+            Abzuege(out int summe, gehaltZwei); // "out int summe" Mit der Deklaration einer neuen Variable
+            Console.WriteLine($"Es wurden von deinem Gehalt mit {gehaltZwei} Euro folgendes abgezogen: {summe} Euro");
+
+            int gehaltDrei = 1700;
+            int abzuege;
+
+            Abzuege(out abzuege, gehaltDrei); // "out abzuege" ohne eine neue Variable zu deklarieren
+            Console.WriteLine($"Es wurden von deinem Gehalt mit {gehaltDrei} Euro folgendes abgezogen: {abzuege} Euro");
+        }
+
+        private static void CallByValue()
+        {
+            void Ausgabe(int x)
+            {
+                x = x + 5;
+                Console.WriteLine("Call by Value macht mit a die Ausgabe " + x + " durch die Kopie x");
+            }
+
+            int a = 5;
+            Ausgabe(a);
+            Console.WriteLine("Aber a selber bleibt unverändert also " + a);
+
+            // Beispiel mit Rückgabewert Call by Value wenn wir den Uhrsprungswert überschreiben wollen
+
+            int Ueberschreiben(int x)
+            {
+                return x = x + 5;
+            }
+
+            a = Ueberschreiben(a);
+            Console.WriteLine(a);
+        }
+    }
+}
